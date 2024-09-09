0b13118f50190a52fdd1fa9f172324a83ebc44a3
Reeshav Acharya <reeshav99acharya@gmail.com>
2024-09-09 18:27:33 +0545

    Add blockfrost configuration file

---
+++ src/blockfrost.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+require("dotenv").config();
+
+const blockfrostSancho = "https://cardano-sanchonet.blockfrost.io";
+const projectId = process.env.BLOCKFROST_SANCHO;
+const apiClient = axios.create({
+  baseURL: blockfrostSancho,
+  headers: {
+    project_id: projectId,
+  },
+});
+
+export async function fetchAssetData(assetId: string) {
+  try {
+    const response = await apiClient.get(`api/v0/assets/${assetId}`);
+    return response.status;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      return true;
+    } else {
+      // Handle other errors
+      console.error("Error fetching data:", error.message);
+      throw error; // Rethrow the error if it is not a 404
+    }
+  }
+}
+export async function fetchUTxOData(address: string) {
+  try {
+    const response = await apiClient.get(`api/v0/addresses/${address}/utxos`);
+    const utxoInfo = response.data;
+    const parsedUTxOs = utxoInfo.map(
+      (utxo: any) => `${utxo.tx_hash}#${utxo.tx_index}`
+    );
+    return parsedUTxOs;
+  } catch (error: any) {}
+}
