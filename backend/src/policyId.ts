59cf6de878ead8993e33defa111888af169f3429
Reeshav Acharya <reeshav99acharya@gmail.com>
2024-09-11 09:02:33 +0545

    Remove 'require' imports

--- backend/src/policyId.ts
+++ backend/src/policyId.ts
@@ -1,12 +1,12 @@
-const CSL = require("@emurgo/cardano-serialization-lib-nodejs/cardano_serialization_lib");
-import CardanoWasm, {
+import {
+  Ed25519KeyHash,
   NativeScript,
   ScriptPubkey,
 } from "@emurgo/cardano-serialization-lib-nodejs";
 
 export function getPolicy(keyBytes: Buffer): string {
   const scriptPubKey = ScriptPubkey.new(
-    CSL.Ed25519KeyHash.from_bytes(keyBytes)
+    Ed25519KeyHash.from_bytes(keyBytes)
   );
   return NativeScript.new_script_pubkey(scriptPubKey).hash().to_hex();
 }
