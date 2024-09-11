1fb7ed792ea8a0cc1b37cf95edfd278fb041f692
Niraj <duwalkneerose@gmail.com>
2024-09-11 14:39:27 +0545

    chore: update grid css

--- frontend/src/components/landing-sections/features.tsx
+++ frontend/src/components/landing-sections/features.tsx
@@ -4,12 +4,12 @@ import FeatureCard from "@src/components/cards/featureCard";
 export default function Features() {
   return (
     <div className="flex justify-center items-center sm:py-[120px] py-[60px]">
-      <div className="flex flex-col max-w-[1440px] px-[96px] sm:px-6">
+      <div className="flex flex-col max-w-[1440px] !px-[96px] sm:px-6">
         <p className="h2 font-bold text-primary-900">{features.title}:</p>
         <p className="h3 font-semibold text-primary-900 mt-2 mb-[120px]">
           {features.description}
         </p>
-        <div className="grid grid-flow-col sm:grid-cols-3 max-w-[1248px] xs:grid-cols-2 grid-cols-1  gap-[56px]">
+        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[56px]">
           {features.steps.map((step) => {
             return <FeatureCard key={step.id} {...step} />;
           })}
