7f8fb17b98a867f372adbf03a99a13c4f18ebb85
Niraj <duwalkneerose@gmail.com>
2024-09-11 12:46:43 +0545

    chore: add banner component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
+import Banner from "@src/components/landing-page/banner";
 
 export default function Home() {
   return (
     <div className="h-full w-full">
-     
+      <Banner />
     </div>
   );
 }
---
+++ frontend/src/constants/index.tsx
@@ -0,0 +1,5 @@
+export const banner = {
+  title: "Create, Manage, and Share Your Unique DRep ID",
+  description:
+    "Streamline your digital identity with a URL-friendly DRep ID. Easily mint and share your unique identifier with simplicity.",
+};
